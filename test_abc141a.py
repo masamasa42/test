510042909919ddb14576e0c07af203b85b295fe7
masamasa42 <rokkaku99@gmail.com>
2019-11-07 16:25:33 +0900

    test

---
+++ test_abc141a.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+#https://atcoder.jp/contests/abc141/tasks/abc141_a
+import sys
+#import collections
+
+#n= int(input())
+s= input()
+#tmp = input().split()
+#a = list(map(lambda a: int(a), tmp))
+
+for i in range(len(s)):
+	if(i%2==0):
+		if(s[i]=="L"):
+			print("No")
+			sys.exit()
+	else:
+		if(s[i]=="R"):
+			print("No")
+			sys.exit()
+		
+print("Yes")
