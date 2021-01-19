7d70037cb402e58248f6f801d386945227467860
masamasa42 <rokkaku99@gmail.com>
2021-01-19 14:11:00 +0900

    abc

--- abc151d.py
+++ abc151d.py
@@ -34,39 +34,3 @@ for i in range(H):
                 ans = max(ans, distance[k][l])
 
 print(ans)
-
-#for i in range(H):
-#    print(maze[i])
-#for i in range(H):
-#    print(distance[i])
-
-
-#n= int(input())
-#s= input()
-#tmp = input().split()
-#n,m = list(map(lambda a: int(a), tmp))
-
-#ans=[]
-#anstry=[]
-
-#for i in range(n):
-#    ans.append(0)
-#    anstry.append(0)
-#for i in range(m):
-#    tmp = input().split()
-#    index=int(tmp[0])
-#    if (tmp[1]=="WA"):
-#        if(ans[index-1]==0):
-#            anstry[index-1]+=1
-#    else:
-#        ans[index-1]=1
-
-#num_ans=0
-#num_try=0
-#for i in range(n):
-#    num_ans+=ans[i]
-#    if(ans[i]==1):
-#        num_try+=anstry[i]
-#out=str(num_ans)+" "+str(num_try)
-#print(out)
-
